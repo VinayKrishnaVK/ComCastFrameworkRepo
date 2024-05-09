cb7e0cc17ff2b89d97fc1c96aaecb43f9da8191c
Vinay krishna <37305617+vinaykrishnahv@users.noreply.github.com>
2024-05-09 17:29:13 +0530

    calenderBtn is added in product page

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/generic/ObjectRepo/ProductPage.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/generic/ObjectRepo/ProductPage.java
@@ -7,6 +7,13 @@ public class ProductPage {
 
 	@FindBy(xpath = "//img[@alt=\"Create Product...\"]")
 	private WebElement ProdPlusBtn;
+	
+	@FindBy(xpath = "//img[@alt='Open Calendar...']")
+	private WebElement CalenderBtn;
+
+	public WebElement getCalenderBtn() {
+		return CalenderBtn;
+	}
 
 	public WebElement getProdPlusBtn() {
 		return ProdPlusBtn;
